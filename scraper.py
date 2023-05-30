452f9e882476efc2fff1ab1970d65e7f31ea28e6
Muhammad Fazeel <126400735+Fazeel355@users.noreply.github.com>
2023-05-30 23:10:07 +0500

    scraper.py

--- scraper.py
+++ scraper.py
@@ -1,20 +1,34 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 import requests
 
-def scrape_books(url):
+def scrape_books():
+    url = "https://books.toscrape.com/catalogue/category/books_1/index.html"
     response = requests.get(url)
-    books = response.json()['books']
+    html = response.text
+
+    # Extracting data using pandas
+    dfs = pd.read_html(html)
+    books_table = dfs[0]
+
+    # Cleaning up the data
+    books_table.columns = books_table.iloc[0]
+    books_table = books_table[1:]
+
+    # Selecting relevant columns
+    books = books_table[['Title', 'Price']]
+
+    # Adding a random availability column using numpy
+    availabilities = ['In Stock', 'Out of Stock']
+    books['Availability'] = np.random.choice(availabilities, size=len(books))
+
     return books
 
 # Streamlit UI
 st.title("Books Scraper")
 
-url = "https://books.toscrape.com/catalogue/category/books_1/index.json"
 if st.button("Scrape"):
-    scraped_data = scrape_books(url)
+    scraped_data = scrape_books()
     st.write("Scraped Data:")
-    for book in scraped_data:
-        st.write(f"Title: {book['title']}")
-        st.write(f"Price: {book['price']}")
-        st.write(f"Availability: {book['availability']}")
-        st.write("---")
+    st.write(scraped_data)
