30b04db35dcbe98ec47ae906aa1f9870b8481cbb
Muhammad Fazeel <126400735+Fazeel355@users.noreply.github.com>
2023-05-30 23:05:50 +0500

    scraper.py

--- scraper.py
+++ scraper.py
@@ -1,32 +1,15 @@
 import streamlit as st
 import requests
-from bs4 import BeautifulSoup
 
 def scrape_books(url):
     response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    books = []
-
-    book_elements = soup.find_all('article', class_='product_pod')
-    for element in book_elements:
-        title = element.find('h3').find('a')['title']
-        price = element.find('p', class_='price_color').text.strip()
-        availability = element.find('p', class_='instock availability').text.strip()
-
-        book = {
-            'title': title,
-            'price': price,
-            'availability': availability
-        }
-        books.append(book)
-
+    books = response.json()['books']
     return books
 
 # Streamlit UI
 st.title("Books Scraper")
 
-url = "https://books.toscrape.com/"
+url = "https://books.toscrape.com/catalogue/category/books_1/index.json"
 if st.button("Scrape"):
     scraped_data = scrape_books(url)
     st.write("Scraped Data:")
