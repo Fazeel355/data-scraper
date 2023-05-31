7e2ba6561d5ad4a83296730ff9054d872a29f5f7
Muhammad Fazeel <126400735+Fazeel355@users.noreply.github.com>
2023-05-31 19:49:41 +0500

    scraper.py

--- scraper.py
+++ scraper.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
 
 @st.cache
 def main():
     st.title("Web Scraper")
-    
+
     # Input field for the URL
     url = st.text_input("Enter the URL to scrape")
 
@@ -13,19 +13,18 @@ def main():
         try:
             response = requests.get(url)
             soup = BeautifulSoup(response.content, "html.parser")
-            
+
             # Scrape data from the website
             # For example, let's scrape all the headlines from an HTML page
             headlines = soup.find_all("h1")
-            
+
             # Display the scraped data
             st.write("Scraped headlines:")
             for headline in headlines:
                 st.write(headline.text)
-            
-        except requests.exceptions.RequestException as e:
+
+        except requests.RequestException as e:
             st.write("Error occurred:", e)
 
 if __name__ == "__main__":
     main()
-
